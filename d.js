0f35c6692e9a0a21729799af09ddb218d5dc196b
Ghasem Kiani <ghasemkiani@gmail.com>
2025-10-15 20:28:51 +0330

    update

--- d.js
+++ d.js
@@ -3,5 +3,9 @@ import D from "decimal.js";
 D.config({ precision: 200 });
 const d = (bi) => D(typeof bi === "bigint" ? String(bi) : bi);
 
+D.prototype.asFixed = function (n) {
+  return this.mul(10 ** n).truncated().div(10 ** n).toFixed(n);
+};
+
 export { d, D };
 export default D;
