8b4a648944499b0651b42aa52aaa6dd0472bd135
Ghasem Kiani <ghasemkiani@gmail.com>
2025-10-15 20:41:07 +0330

    update

--- d.js
+++ d.js
@@ -4,7 +4,7 @@ D.config({ precision: 200 });
 const d = (bi) => D(typeof bi === "bigint" ? String(bi) : bi);
 
 D.prototype.asFixed = function (n) {
-  return this.mul(10 ** n).truncated().div(10 ** n).toFixed(n);
+  return this.toFixed(n, D.ROUND_DOWN);
 };
 
 export { d, D };
