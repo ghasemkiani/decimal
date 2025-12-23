aa3a54c04ff1e7c4e5d910248166e0fbab3988e1
Ghasem Kiani <ghasemkiani@gmail.com>
2025-12-24 00:42:49 +0330

    update

--- d.js
+++ d.js
@@ -1,11 +1,11 @@
 import D from "decimal.js";
 
-BigInt.prototype['toJSON'] = function () { return this.toString() }
+BigInt.prototype["toJSON"] = function () { return this.toString() }
 
 D.config({ precision: 200 });
 const d = (bi) => D(typeof bi === "bigint" ? String(bi) : bi);
 
-D.prototype.asFixed = function (n) {
+D.prototype.asFixed = function (n = 0) {
   return this.toFixed(n, D.ROUND_DOWN);
 };
 
